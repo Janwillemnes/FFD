bbc08be03e6e1b24d5d10f75ee72395a0bd46650
Janwillemnes <Janwillem.nes@gmail.com>
2018-03-13 10:04:42 +0100

    Favorites counter
    Functional feedback when adding a movie to your favorites.

--- movieapp/js/script.js
+++ movieapp/js/script.js
@@ -1,6 +1,7 @@
-heartList = document.querySelectorAll(".heart");
+var heartList = document.querySelectorAll(".heart");
+var visualCounter = document.getElementById("favcount");
+var favTotal = 0;
 
-console.log(heartList)
 
 for(var i =0; i<heartList.length; i++){
 	heartList[i].addEventListener("click", like);
@@ -11,10 +12,31 @@ function like(event){
 	if (this.data=="unliked") {
 		this.src="images/heart-fill.svg";
 		this.data="liked";
+		visualCounter.classList.toggle("bounceIn");
+		countup();
 	
 	} else {
 		this.src="images/heart-outline.svg";
 		this.data="unliked";
+		visualCounter.classList.toggle("bounceIn");
+		countdown();
 	}
 		
 }
+
+function countup(){
+	favTotal++;
+	favCounter();	
+}
+
+function countdown(){
+	favTotal--;
+	favCounter();
+}
+
+function favCounter(){
+	visualCounter.innerHTML = favTotal;
+	
+}
+
+ 
